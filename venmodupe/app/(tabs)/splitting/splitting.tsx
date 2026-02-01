cc6c358e5f0fc3917bff8a7e59bc7a0910aefb35
Rachael Chen <rchen2027@berkeley.edu>
2026-01-31 17:39:37 -0800

    add packages

---
+++ venmodupe/app/(tabs)/splitting/splitting.tsx
@@ -0,0 +1,12 @@
+// sub_total = subtotal of all food items
+// total = total of food items
+// num_people = number of people at meal
+
+// amount_owed
+// food_item / sub_total = amount_owed / total
+// user must input sub_total (for exact price of amount_owed)
+
+// amount_owed = food_item * total / sub_total
+
+
+// general_owed = total / num_people
